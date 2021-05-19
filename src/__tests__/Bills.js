f4123130b97aa43a36a0719281d6ef2c38e9de16
Audrey Diez <audrey.diez@gmail.com>
2021-05-19 15:44:24 +0200

    test: TU When I navigate to Bills UI - fetch tests

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -11,6 +11,8 @@ import Firestore from "../app/Firestore";
 import DashboardFormUI from "../views/DashboardFormUI";
 import Dashboard from "../containers/Dashboard";
 import userEvent from "@testing-library/user-event";
+import firebase from "../__mocks__/firebase";
+import DashboardUI from "../views/DashboardUI";
 
 const data = []
 const loading = false
@@ -131,5 +133,34 @@ describe("Given I am connected as an employee", () => {
 
     })
   })
+  describe("When I navigate to Bills UI", () => {
+    test("fetches bills from mock API GET", async () => {
+      const getSpy = jest.spyOn(firebase, "get")
+      const bills = await firebase.get()
+      expect(getSpy).toHaveBeenCalledTimes(1)
+      expect(bills.data.length).toBe(4)
+    })
+    test("fetches bills from an API and fails with 404 message error", async () => {
+      firebase.get.mockImplementationOnce(() =>
+          Promise.reject(new Error("Erreur 404"))
+      )
+      const html = BillsUI({ error: "Erreur 404" })
+      document.body.innerHTML = html
+      const message = await screen.getByText(/Erreur 404/)
+      expect(message).toBeTruthy()
+    })
+    test("fetches messages from an API and fails with 500 message error", async () => {
+      firebase.get.mockImplementationOnce(() =>
+          Promise.reject(new Error("Erreur 500"))
+      )
+      const html = BillsUI({ error: "Erreur 500" })
+      document.body.innerHTML = html
+      const message = await screen.getByText(/Erreur 500/)
+      expect(message).toBeTruthy()
+    })
+  })
+
+
 })
+
 
