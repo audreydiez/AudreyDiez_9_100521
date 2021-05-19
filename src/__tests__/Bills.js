0a5654a15cd8d5d9a0560cf6994e394ad1b6f748
Audrey Diez <audrey.diez@gmail.com>
2021-05-19 11:06:59 +0200

    test: TU Then bill icon in vertical layout should be highlighted

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,18 +1,47 @@
 import {fireEvent, screen} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { Router } from "../app/Router"
+import Router from "../app/Router.js"
 import {ROUTES, ROUTES_PATH} from "../constants/routes";
 import Bills from "../containers/Bills";
 import firestore from "../app/Firestore";
 import {localStorageMock} from "../__mocks__/localStorage";
+import VerticalLayout from "../views/VerticalLayout";
+import Firestore from "../app/Firestore";
+
+const data = []
+const loading = false
+const error = null
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: []})
+
+      jest.mock("../app/Firestore");
+      Firestore.bills = () => ({ bills, get: jest.fn().mockResolvedValue() });
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+
+      const pathname = ROUTES_PATH['Bills']
+      const html = ROUTES({
+        pathname,
+        data,
+        loading,
+        error
+      })
       document.body.innerHTML = html
-      //to-do write expect expression
+
+      Object.defineProperty(window, "location", { value: { hash: pathname } });
+      document.body.innerHTML = `<div id="root"></div>`;
+      Router()
+
+      expect(
+          screen.getByTestId("icon-window").classList.contains("active-icon")
+      ).toBeTruthy()
+
+
     })
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills })
@@ -36,7 +65,7 @@ describe("Given I am connected as an employee", () => {
 
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       window.localStorage.setItem('user', JSON.stringify({
-        type: 'employee'
+        type: 'Employee'
       }))
       //window.addEventListener = jest.fn();
       const bills = new Bills({ document, onNavigate, firestore, localStorage  })
